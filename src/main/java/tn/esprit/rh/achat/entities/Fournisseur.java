dda0873bf2707472909e2219e3ed0484e93b4afe
doraaaaaaaaaa <100046845+doraaaaaaaaaa@users.noreply.github.com>
2024-08-09 16:42:54 +0200

    Update Fournisseur.java

--- src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -1,23 +1,24 @@
 package tn.esprit.rh.achat.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.Set;
 import javax.persistence.CascadeType;
-import javax.persistence.Entity;
+import javax.persistence.Entity;  // Correct import
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+// Other imports
 
 @Entity
 @Getter
@@ -33,17 +34,6 @@ public class Fournisseur implements Serializable {
 	private Long idFournisseur;
 	private String code;
 	private String libelle;
-	@Enumerated(EnumType.STRING)
-	private CategorieFournisseur  categorieFournisseur;
-	@OneToMany(mappedBy="fournisseur")
-	@JsonIgnore
-	private Set<Facture> factures;
-    @ManyToMany
-    @JsonIgnore
-    private Set<SecteurActivite> secteurActivites;
-    @OneToOne(cascade= CascadeType.ALL,fetch=FetchType.EAGER)
-    private DetailFournisseur detailFournisseur;
-    
 
-	
+
 }
